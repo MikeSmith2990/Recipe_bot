e6a72a51964cbb3dac73918929d0501b6d2cc510
Mike Smith <36375212+MikeSmith2990@users.noreply.github.com>
2023-01-15 16:29:56 -0500

    Refactor how reddit scraper module was returning posts objects, inform user request is processing when callback received

--- app.py
+++ app.py
@@ -51,6 +51,9 @@ class BotApp:
         random_handler = CommandHandler('random', self.random)
         self.bot_app.add_handler(random_handler)
 
+        test_handler = CommandHandler('test', self.test)
+        self.bot_app.add_handler(test_handler)
+
     async def start(self, update, context):
         await context.bot.send_message(chat_id=update.effective_chat.id,
                                        text="Hi! Please request some posts with `/list <num posts 1-25>` \n" +
@@ -59,19 +62,35 @@ class BotApp:
                                        "Right now I only support r/GifRecipes.",
                                        )
 
+    async def test(self, update, context):
+        chat_id = update.effective_chat.id
+        print("trying to grab image from second post")
+        post_list = self.reddit.get_sorted_hot_posts(5)
+        post = post_list[2]
+        print("Post list:")
+        print(str(post_list))
+        print("post 2?")
+        print(str(post))
+
     async def random(self, update, context):
         chat_id = update.effective_chat.id
+        print("Got /random from: " + str(chat_id))
+
+        await context.bot.send_message(chat_id=chat_id, text="Got request for random post. Processing...")
+
         post_list = self.reddit.get_sorted_hot_posts(5)
         val = random.randint(0, 4)
 
         recipe_text = self.get_recipe_from_post(post_list[val])
-        await context.bot.send_message(chat_id=chat_id, text="Heres your random recipe result: " + str(post_list[val][1]))
-        if not post_list[val][2].is_self:
-            try:
-                await context.bot.send_animation(chat_id=chat_id, animation=post_list[val][2].url)
-            except:
-                pass
-        await context.bot.send_message(chat_id=chat_id, text=str(recipe_text))
+        await context.bot.send_message(chat_id=chat_id,
+                                       text="Heres your random recipe result: " + str(post_list[val]["title"]))
+
+        await context.bot.send_message(chat_id=chat_id,
+                                       text="reddit.com" + str(post_list[val]["post"].permalink))
+
+        await context.bot.send_message(chat_id=chat_id,
+                                       disable_web_page_preview=True,
+                                       text=str(recipe_text))
         return
 
     async def list(self, update, context):
@@ -79,7 +98,7 @@ class BotApp:
         chat_id = update.effective_chat.id
         try:
             num = int(context.args[0])
-            print("Got /recipe " + str(num) + " from: " + str(chat_id))
+            print("Got /list " + str(num) + " from: " + str(chat_id))
         except:
             await context.bot.send_message(chat_id=chat_id, text="Sorry, couldn't parse that request.")
             return
@@ -88,20 +107,25 @@ class BotApp:
             return
 
         await context.bot.send_message(chat_id=chat_id,
-                                 text="Heres the highest " + str(num) + " scoring posts on r/GifRecipes right now")
+                                       text="Heres the highest " + str(num) +
+                                            " scoring posts on r/GifRecipes right now")
 
         post_list = self.reddit.get_sorted_hot_posts(num=num)
 
         self.user_post_dict[chat_id] = post_list
 
         for post in post_list:
-            await context.bot.send_message(chat_id=chat_id, text=str(post[1]) + " - " + str(post[0]) + " Points")
+            await context.bot.send_message(chat_id=chat_id, text=str(post["title"]) + " - " +
+                                                                 str(post["score"]) + " Points")
 
     async def recipe(self, update, context):
         search_text = None
         chat_id = update.effective_chat.id
+        await context.bot.send_message(chat_id=chat_id, text="Got your request. Processing...")
+
         try:
             search_text = ' '.join(context.args)
+            print("Got /list " + str(search_text) + " from: " + str(chat_id))
         except:
             await context.bot.send_message(chat_id=chat_id, text="Sorry, couldn't parse that request")
             return
@@ -116,27 +140,30 @@ class BotApp:
         except KeyError:
             post_list = self.reddit.get_sorted_hot_posts()
 
-        # attempt to match search to post list
+        # attempt to match search to post list,
+        # returns first result that has the search text inside the post title
         for post in post_list:
-            if search_text in post[1].lower():
+            if search_text in post["title"].lower():
                 recipe_text = self.get_recipe_from_post(post)
-                await context.bot.send_message(chat_id=chat_id, text="Heres the recipe result for " + str(post[1]))
-                if not post[2].is_self:
-                    try:
-                        await context.bot.send_animation(chat_id=chat_id, animation=post[2].url)
-                    except:
-                        pass
-                await context.bot.send_message(chat_id=chat_id, text=str(recipe_text))
+                await context.bot.send_message(chat_id=chat_id,
+                                               text="Heres the recipe result for " + str(post["title"]))
+                # send the gif link
+                await context.bot.send_message(chat_id=chat_id,
+                                             text="reddit.com" + str(post["post"].permalink))
+                #send recipe
+                await context.bot.send_message(chat_id=chat_id,
+                                               disable_web_page_preview=True,
+                                               text=str(recipe_text))
                 return
 
         await context.bot.send_message(chat_id=chat_id,
-                                 text="Couldn't find that post title among your last post request")
+                                       text="Couldn't find that post title among your last post request")
 
-    def get_recipe_from_post(self, post_tup=None):
-        if post_tup is None:
+    def get_recipe_from_post(self, post_dict=None):
+        if post_dict is None:
             return None
 
-        post = post_tup[2]
+        post = post_dict["post"]
         comments = post.comments
         comments.replace_more(limit=1)
         first_comment = comments[0]  # should be the sticky comment...
@@ -146,3 +173,12 @@ class BotApp:
             recipe_comment = recipe
             break
         return recipe_comment.body
+
+    def get_post_image(self, post_tup=None):
+        if post_tup is None:
+            return None
+
+        post = post_tup[2]
+        print(post.url)
+
+
--- reddit_scrape.py
+++ reddit_scrape.py
@@ -26,11 +26,11 @@ class RedditScrape:
 
     def get_sorted_hot_posts(self, num=10, flair=None):
         # Gets number of posts from gifrecipe hot list, sorted by score. Default is 10
-        # returns tuple of (score, post title, post object, post flair)
+        # returns dict of Post object, post score, post title, post flair
 
         # if no posts requested, return
         if num == 0:
-            return
+            return None
 
         post_list = list()
         count = 0
@@ -39,12 +39,17 @@ class RedditScrape:
             if post.stickied or post.is_self or (post.link_flair_text != flair and flair is not None):
                 continue
             # add to tuple...
-            post_list.append((post.score, post.title, post, post.link_flair_text))
+            post_list.append({
+                "post": post,
+                "score": post.score,
+                "title": post.title,
+                "flair": post.link_flair_text
+                             })
             count += 1
             if count == num:
                 break
 
         # sort them by score
-        sorted_post_list = sorted(post_list, key=lambda post: post[0])
+        sorted_post_list = sorted(post_list, key=lambda post: post["score"])
         sorted_post_list.reverse()
         return sorted_post_list
